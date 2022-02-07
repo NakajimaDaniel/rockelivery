7228f45d9350540af224d4be9e83a6946e19501f
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-02-07 19:42:34 -0300

    feat: get route and show user data

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -7,6 +7,8 @@ defmodule Rockelivery do
   if it comes from the database, an external API or others.
   """
   alias Rockelivery.Users.Create, as: UserCreate
+  alias Rockelivery.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate get_user_by_id(id), to: UserGet, as: :by_id
 end
