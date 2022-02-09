30e5a479ccc6a00092fc75578f6e1fa8c705e6fa
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-02-09 19:32:57 -0300

    fix: transform error in struct

--- lib/rockelivery/users/get.ex
+++ lib/rockelivery/users/get.ex
@@ -1,18 +1,18 @@
 defmodule Rockelivery.Users.Get do
-  alias Rockelivery.{Repo, User}
+  alias Rockelivery.{Error, Repo, User}
 
   alias Ecto.UUID
 
   def by_id(id) do
     case UUID.cast(id) do
-      :error -> {:error, %{status: :bad_request, result: "invalid id format"}}
+      :error -> {:error, Error.build_id_format_error()}
       {:ok, uuid} -> get(uuid)
     end
   end
 
   defp get(uuid) do
     case Repo.get(User, uuid) do
-      nil -> {:error, %{status: :not_found, result: "User not found"}}
+      nil -> {:error, Error.build_user_not_found_error()}
       user -> {:ok, user}
     end
   end
